55c2ce60f3e3d0d78d4c038904dbd86e4853592a
10636 <10636@DESKTOP-M4VE8AI>
2019-12-04 21:46:39 +0800

    best

--- liu/cv_params.py
+++ liu/cv_params.py
@@ -6,69 +6,84 @@ Created on 2019年11月26日
 @author: liushouhua
 '''
 import pandas as pd
+from sklearn.model_selection import KFold
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from main import data_review,myFevalAuc
+from sklearn.metrics import auc, roc_curve
+from liu.main2 import data_review
+def myFevalAuc(preds, xgb_label):
+    fpr, tpr, _ = roc_curve(xgb_label, preds, pos_label=1)
+    return auc(fpr, tpr)
 
 def grid_search(param_grid):
     params_xgb = {
         'booster':'gbtree',
         'objective': 'binary:logistic',
         'eval_metric':'auc',
-        
-#         'gamma':0.2,        #损失下降多少才分裂
         'min_child_weight':3,
-        'max_depth':8,
+        "min_child_samples":50,
+        "max_bin":70,
+        'colsample_bytree':0.7, #生成树时特征采样比例
+        'colsample_bylevel':0.7, #表示树的每层划分的特征采样比例
+        'eta': 0.01,
+        'max_depth':5,
         'lambda':2,     #控制模型复杂度的权重值的L2曾泽化参数，参数越大越不容易过拟合
-        'subsample':0.85,    #随机采样的训练样本
-        'colsample_bytree':0.85, #生成树时特征采样比例
-        'colsample_bylevel':0.85, #表示树的每层划分的特征采样比例
-        'eta': 0.02,
-        
         'tree_method':'hist', #表示树的构建方法，确切来说是切分点的选择算法，'gpu_hist' or 'gpu_exact'包括贪心算法，近似贪心算法，直方图算法
         'seed':1000,
-        'silent': 0,    #信息输出设置成1则没有信息输出
+        'silent': True,    #信息输出设置成1则没有信息输出
         'nthread':12
         }
     
-    X_data = data_review()    
-    drop_labels= ["ID","经营期限至","邮政编码","核准日期","行业代码","注销时间","经营期限自","成立日期","经营范围"]
-    X_data.drop(drop_labels,axis=1,inplace=True)
+#     data = pd.read_csv( "data.csv", encoding='utf-8',low_memory=False)
+#     X_data = data.drop(["Label", "ID"], axis=1).values[:-9578]
+#     y_label = data.Label.values[:-9578]
     
-    train_data, test_data = train_test_split(X_data, test_size=0.2, random_state=0)
-
-    train_label = train_data["Label"]
-    train_data = train_data.drop(["Label"],axis=1)
-
-    valid_label = test_data["Label"]
-    valid_data = test_data.drop(["Label"],axis=1)
-
-    xgb_train = xgb.DMatrix(train_data, label=train_label)
-    xgb_valid = xgb.DMatrix(valid_data, label=valid_label)
-    evallist = [(xgb_train, 'train'),(xgb_valid, 'eval')]
+    
+    X_data, _ = data_review()
+    y_label = X_data["Label"]
+    drop_labels= ["ID","Label"] 
+    X_data.drop(drop_labels,axis=1,inplace=True)
     
     best_score = -1
     best_param = {}
+    iters = 0
+    kf = KFold(n_splits = 5, shuffle=True, random_state=520)
     for i in param_grid[0][1]:
         p_name0 = param_grid[0][0]
         for j in param_grid[1][1]:
             p_name1 = param_grid[1][0]
             for k in param_grid[2][1]:
+                iters += 1
                 p_name2 = param_grid[2][0]
                 params_xgb[p_name0]=i
                 params_xgb[p_name1]=j
                 params_xgb[p_name2]=k
-                cgb_model = xgb.train(params_xgb, xgb_train, num_boost_round=3000 , evals=evallist, verbose_eval=600, early_stopping_rounds=400)
-                vaild_pre = cgb_model.predict(xgb_valid, ntree_limit=cgb_model.best_ntree_limit)
-                auc = myFevalAuc(vaild_pre,valid_label)
-                print("@@@@@@")
-                print(auc,params_xgb[p_name0],params_xgb[p_name1],params_xgb[p_name2])
-                if auc >= best_score:
-                    best_score = auc
+                score = []
+                for _, (train_index, test_index) in enumerate(kf.split(X_data)):
+                   
+                    train_data = X_data.iloc[train_index]
+                    train_label = y_label.iloc[train_index]
+                    valid_data = X_data.iloc[test_index]
+                    valid_label = y_label.iloc[test_index]
+                    xgb_train = xgb.DMatrix(train_data, label=train_label)
+                    xgb_valid = xgb.DMatrix(valid_data, valid_label)
+                    evallist = [(xgb_train, 'train'),(xgb_valid, 'eval')]
+                    cgb_model = xgb.train(params_xgb, xgb_train, num_boost_round=3000, evals=evallist, verbose_eval=0, early_stopping_rounds=500)
+                    vaild_pre = cgb_model.predict(xgb_valid, ntree_limit=cgb_model.best_ntree_limit)
+                    sauc = myFevalAuc(vaild_pre,valid_label)
+                    score.append(sauc)
+                auc_ave = sum(score)/len(score)
+                print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                print("当前得分:",auc_ave," iters: ",iters)
+                print(auc_ave,p_name0,params_xgb[p_name0],p_name1,params_xgb[p_name1],p_name2,params_xgb[p_name2])
+                print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                
+                if auc_ave >= best_score:
+                    best_score = auc_ave
                     best_param = params_xgb
-                    print("---------------------------")
+                    print("************************************************************************************")
                     print("score: ",best_score," param: ",best_param)
-                    print("---------------------------")
+                    print("************************************************************************************")
                     
     print("##################################")
     print("score: ",best_score," param: ",best_param)
@@ -77,9 +92,9 @@ def grid_search(param_grid):
 
 if __name__ == "__main__":
 
-    param_grid = {"lambda":[4,5],"eta":[0.015,0.02,0.025],"max_depth":[7,8,9]}
+    param_grid = [("max_bin",[60, 70, 80]), ("eta",[0.008, 0.01, 0.013 ]), ("lambda",[0.8, 1, 1.5, 2])]
     print()
-    grid_search(list(param_grid.items()))
+    grid_search(param_grid)
     
     
     
--- liu/main.py
+++ liu/main.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 #!/usr/bin/python3
 '''
+单模0.93239
 Created on 2019年11月26日
 
 @author: liushouhua
@@ -12,95 +13,106 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option("display.max_colwidth",100)
 pd.set_option('display.width',1000)
-
 import os
-import sys
-
-import xlrd
-from sklearn.metrics import auc, roc_curve
 import numpy as np
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 from xgboost import plot_importance
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import auc, roc_curve, roc_auc_score
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from tqdm import tqdm
 import matplotlib.pyplot as plt 
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import roc_auc_score
 data = "data/"
-def get_leaf(x_train, y_train, x_val):
-    
+def get_leaf(x_train, y_train, x_val,mln):
     x_train, y_train, x_val = map(np.array, [x_train, y_train, x_val])
     x_train = x_train.reshape(-1, 1)
     y_train = y_train.reshape(-1, 1)
     x_val = x_val.reshape(-1, 1)
-    m = DecisionTreeClassifier(min_samples_leaf=0.001, max_leaf_nodes=10)
+    m = DecisionTreeClassifier(min_samples_leaf=9, max_leaf_nodes=mln)
     m.fit(x_train, y_train)
     return m.apply(x_val), roc_auc_score(y_train, m.predict_proba(x_train)[:, 1])
 
-def business(buss):
-    buss = buss[1:-1].split(",")
-    return len(buss)
+def change_time(x):
+    try:
+        months, days = str(x)[:5].split(":")
+        result = int(months) * 60 + int(days)
+    except ValueError:
+        result = -1
+    return result
 
 def data_review():
     
-#     data_deal(istrain=True)
-    df_train = pd.read_csv(data + "train_deal.csv", encoding='utf-8',low_memory=False)
-#     data_deal(istrain=False)
-    df_test = pd.read_csv(data + "test_deal.csv", encoding='utf-8',low_memory=False)
-    
-    
-    features = ['企业类型',  '登记机关', '行业门类', '管辖机关']
-    target_name= "Label"
-    id_name = "ID"
-    
-    df_dt = pd.concat([df_train, df_test], sort=False, ignore_index=True).fillna(-1)
-    for feature in features:
-        df_dt[feature] = LabelEncoder().fit_transform(df_dt[feature].astype(np.str))
-        df_dt[feature], auc = get_leaf(df_dt[feature].iloc[:-len(df_test)], df_dt[target_name].iloc[:-len(df_test)], df_dt[feature])
-        count_table = pd.pivot_table(df_dt,
-                                     index=feature,
-                                     columns=target_name,
-                                     values=id_name,
-                                     aggfunc="count",
-                                     fill_value=0)
-        
-        count_table["total"] = count_table[0] + count_table[1]
-        count_table["rate"] = count_table[1] / np.sum(count_table[1]) - count_table[0] / np.sum(count_table[0])
-        count_table = count_table.sort_values(by="rate")
-        count_table["label"] = range(len(count_table))
-        df_dt[feature] = df_dt[feature].replace(count_table.index, count_table.label)
-        
-    for feature in features:
-        count_table = pd.pivot_table(df_dt,
-                                     index=feature,
-                                     columns=target_name,
-                                     values=id_name,
-                                     aggfunc="count",
-                                     fill_value=0)
-        count_table[[1, 0]] = count_table[[1, 0]] + 1
-        count_table["rate_pos"] = count_table[1] / np.sum(count_table[1]) * 100
-        count_table["rate_neg"] = count_table[0] / np.sum(count_table[0]) * 100
-        count_table["efficiency"] = count_table["rate_pos"] - count_table["rate_neg"]
-        count_table["rate"] = count_table[1] / (count_table[1] + count_table[0])
-        count_table["woe"] = np.log(count_table["rate_pos"] / count_table["rate_neg"])
-        count_table["iv"] = count_table["woe"] * count_table["efficiency"]
-        count_table.drop([-1, 0, 1, "rate_pos", "rate_neg"], axis=1, inplace=True)
-        count_table.columns = [feature + i for i in count_table.columns]
-        df_dt = df_dt.merge(count_table.reset_index(), left_on=feature, right_on=feature, how="left").drop(feature, axis=1)
+#     if not os.path.exists(data+"train_use.csv"):
+    if True:
+        data_deal(istrain=True)
+        df_train = pd.read_csv(data + "train_deal.csv", encoding='utf-8',low_memory=False)
+        data_deal(istrain=False)
+        df_test = pd.read_csv(data + "test_deal.csv", encoding='utf-8',low_memory=False)
 
-    train_d = df_dt.iloc[:-len(df_test)]
-    test_d = df_dt.iloc[-len(df_test):]
+        target_name= "Label"
+        id_name = "ID"
+        df_dt = pd.concat([df_train, df_test], sort=False, ignore_index=True)
+
+               
+        cat_features = {'企业类型': 49, 
+                        '登记机关': 14, '邮政编码': 80, '行业代码': 248, '行业门类': 18, '管辖机关': 14,  '经营范围': 60, 
+                        '企业状态': 5, '企业类别': 5, 
+                        '经营期限自':25, '经营期限至':25, '成立日期':25, '核准日期':25,
+                        '注销时间': 21,
+                        }
+        time_features = ['经营期限自', '经营期限至', '成立日期', '核准日期',"注销时间"]
+        for tf in time_features:
+            df_dt[tf] = df_dt[tf].apply(lambda x: change_time(x))
+            
+        for feature,leaf_num in cat_features.items():
+            if leaf_num > 25:
+                leaf_num = 25
+            if feature not in time_features:
+                df_dt[feature] = LabelEncoder().fit_transform(df_dt[feature].astype(np.str)) 
+            df_dt[feature], sc = get_leaf(df_dt[feature].iloc[:-len(df_test)], df_dt[target_name].iloc[:-len(df_test)], df_dt[feature],leaf_num)
+
+            count_table = pd.pivot_table(df_dt,
+                                         index=feature,
+                                         columns=target_name,
+                                         values=id_name,
+                                         aggfunc="count",
+                                         fill_value=0)
+            count_table[[1, 0]] = count_table[[1, 0]] + 1
+            count_table["rate_pos"] = count_table[1] / np.sum(count_table[1]) * 100
+            count_table["rate_neg"] = count_table[0] / np.sum(count_table[0]) * 100
+            count_table["efficiency"] = count_table["rate_pos"] - count_table["rate_neg"]
+            count_table["rate"] = count_table[1] / (count_table[1] + count_table[0])
+            count_table["woe"] = np.log(count_table["rate_pos"] / count_table["rate_neg"])
+            count_table["iv"] = count_table["woe"] * count_table["efficiency"]
+            count_table.drop([ 0, 1, "rate_pos", "rate_neg"], axis=1, inplace=True)
+            count_table.columns = [feature + i for i in count_table.columns]
+            df_dt = df_dt.merge(count_table.reset_index(), left_on=feature, right_on=feature, how="left").drop(feature, axis=1)
+
+        # 处理综合数值型数据
+        before_feature = list(set([var for var in df_dt.columns if "年初数" in var]))
+        before_feature.sort()
+        after_feature = list(set([var for var in df_dt.columns if "年末数" in var]))
+        after_feature.sort()
+        delta_feature = df_dt[after_feature].values - df_dt[before_feature].values
+        delta_feature = pd.DataFrame(delta_feature, columns=[var.strip("年初数") + "delta" for var in before_feature])
+        df_dt = df_dt.join(delta_feature)
+     
+        train_d = df_dt.iloc[:-len(df_test)]
+        test_d = df_dt.iloc[-len(df_test):]
+        
+        train_d.to_csv(data+"train_use.csv",index=None,encoding='utf-8')
+        test_d.to_csv(data+"test_use.csv",index=None,encoding='utf-8')
+    
+    train_d = pd.read_csv(data+"train_use.csv",encoding='utf-8',low_memory=False)
+    test_d = pd.read_csv(data+"test_use.csv",encoding='utf-8',low_memory=False)
     return train_d,test_d
-
 
 def knn_miss(data_train,data_test,miss_clo):
     
@@ -145,18 +157,25 @@ def knn_miss(data_train,data_test,miss_clo):
 
 def data_deal(istrain=True):
     #["印花税","注册资本","增值税","企业所得税","教育费","城建税"]
-#     istrain=False
     df_train = pd.read_csv(data + "train.csv", encoding='utf-8',low_memory=False)
-    df_test = pd.read_csv(data + "test.csv", encoding='utf-8',low_memory=False)
-    columns = ["印花税","注册资本","增值税",]
-#     for c in columns:
-#         df_train,df_test = knn_miss(df_train,df_test,c)
+
+#     #去除重复数据
+#     features = [var for var in df_train.columns if var not in ["ID","Label"]]
+#     df = df_train.drop_duplicates(subset=features, keep=False)["ID"].values
+#     baddf = df_train[~df_train["ID"].isin(df)]
+#     train_lable = pd.read_csv(data + "train_label.csv", encoding='utf-8')
+#     baddf = pd.merge(train_lable,baddf,on=['ID'],how='right')
+#     bad_ids = baddf[baddf["Label"]==0]["ID"].values
+#     df_train = df_train[~df_train["ID"].isin(bad_ids)]
+
     if istrain:
         df = df_train.copy()
     else:
-        df = df_test.copy()
+        df = pd.read_csv(data + "test.csv", encoding='utf-8',low_memory=False)
     
-    df["经营范围数量"] = df["经营范围"].apply(business)
+
+    df["经营范围"] = df["经营范围"].apply(lambda x: x.count(","))
+    df["经营具体数目"] =  df["经营范围"]
     df["企业缴税"] = df["增值税"]+df["企业所得税"]+df["印花税"]+df["教育费"]+df["城建税"]
     df["增值税/企业缴税"] = df["增值税"]/df["企业缴税"]
     df["企业所得税/企业缴税"] = df["企业所得税"]/df["企业缴税"]
@@ -165,58 +184,101 @@ def data_deal(istrain=True):
     df["城建税/企业缴税"] = df["城建税"]/df["企业缴税"]
     df["教育费/城建税"] = df["教育费"]/df["城建税"]
     df["教育费/增值税"] = df["教育费"]/df["增值税"]
+    
     df["企业所得税/投资总额"] = df["企业所得税"]/df["投资总额"]
-    df["增值税/经营范围数量"] = df["增值税"]/df["经营范围数量"]
-    df["注册资本/增值税"] = df["注册资本"]/df["增值税"]
+    df["企业所得税/注册资本"] = df["企业所得税"]/df["注册资本"]
+    df["增值税/注册资本"] = df["增值税"]/df["注册资本"]
+    df["增值税/投资总额"] = df["增值税"]/df["投资总额"]
+    df["企业缴税/投资总额"] = df["企业缴税"]/df["投资总额"]
+    df["企业缴税/注册资本"] = df["企业缴税"]/df["注册资本"]
+    
+    df["企业缴税/经营具体数目"] = df["企业缴税"]/df["经营具体数目"]
+    df["投资总额/经营具体数目"] = df['投资总额']/df['经营具体数目']
+    df["注册资本/经营具体数目"] = df['注册资本']/df['经营具体数目']
+    
+    df["投资总额/注册资本"] = df['投资总额']/df['注册资本']
         
     if istrain:
         train_lable = pd.read_csv(data + "train_label.csv", encoding='utf-8')
-        df = pd.merge(train_lable,df,on=['ID'],how='left')    
+        df = pd.merge(train_lable,df,on=['ID'],how='right')    
         df.to_csv(data+"train_deal.csv",index=None,encoding='utf_8_sig')
     else:
         df.to_csv(data+"test_deal.csv",index=None,encoding='utf_8_sig')
 
 def xgb_model():
-    global params_xgb
+    params_xgb = {
+        'booster':'gbtree',
+        'objective': 'binary:logistic',
+        'eval_metric':'auc',
+        'min_child_weight':3,
+        "min_child_samples":50,
+        "max_bin":60,
+        'colsample_bytree':0.7, #生成树时特征采样比例
+        'colsample_bylevel':0.7, #表示树的每层划分的特征采样比例
+        'eta': 0.01,
+        'max_depth':5,
+        'lambda':2,     #控制模型复杂度的权重值的L2曾泽化参数，参数越大越不容易过拟合
+        'tree_method':'hist', #表示树的构建方法，确切来说是切分点的选择算法，'gpu_hist' or 'gpu_exact'包括贪心算法，近似贪心算法，直方图算法
+        'seed':1000,
+        'silent': True,    #信息输出设置成1则没有信息输出
+        'nthread':12
+        }
     X_data,test_x = data_review()
     y_label = X_data["Label"]
-    
+    print(X_data.shape)
+
     sub = test_x[["ID"]].copy()
     
-    drop_labels= ["ID","经营期限至","邮政编码","核准日期","行业代码","注销时间","经营期限自","成立日期","经营范围","Label"] #+ [ "登记机关", "企业状态", "企业类别", "管辖机关"]
+    drop_labels= ["ID","Label"] 
     X_data.drop(drop_labels,axis=1,inplace=True)
     test_x.drop(drop_labels,axis=1,inplace=True)
 
     res = []
-    kf = KFold(n_splits = 5, shuffle=True, random_state=1000)
     score = []
-    for i, (train_index, test_index) in enumerate(kf.split(X_data)):
-        print('第{}次训练...'.format(i+1))
-       
-        train_data = X_data.iloc[train_index]
-        train_label = y_label.iloc[train_index]
-          
-        valid_data = X_data.iloc[test_index]
-        valid_label = y_label.iloc[test_index]
-
-        xgb_train = xgb.DMatrix(train_data, label=train_label)
-        xgb_valid = xgb.DMatrix(valid_data, valid_label)
-        evallist = [(xgb_train, 'train'),(xgb_valid, 'eval')]
-        cgb_model = xgb.train(params_xgb, xgb_train, num_boost_round=3000, evals=evallist, verbose_eval=300, early_stopping_rounds=400)
+#     from lightgbm import LGBMClassifier
+#     params_initial = {
+#     'objective': 'binary',
+#     "learning_rate":0.01,
+#     'metric': 'auc',
+#     'num_leaves': 32,
+#     'max_bin': 50,
+#     'max_depth': 5,
+#     'min_child_samples': 50,
+#     'min_child_weight': 3,
+#     'n_jobs': -1,
+#     }
+    for iter in range(100):
+        print("iter %02d" % (iter+1))
+        train_data, valid_data, train_label, valid_label = train_test_split(X_data, y_label, test_size=0.3, random_state=100+iter)
+#         x_train, x_test, y_train, y_test = train_test_split(X_data, y_label, test_size=0.4, random_state=iter)
+#         clf = LGBMClassifier(n_estimators=1000, **params_initial)
+#         clf.fit(x_train, y_train, eval_set=[(x_test, y_test)], verbose=100, early_stopping_rounds=500)
         
+        xgb_train = xgb.DMatrix(train_data, label=train_label,silent=True)
+        xgb_valid = xgb.DMatrix(valid_data, valid_label,silent=True)
+        evallist = [(xgb_train, 'train'),(xgb_valid, 'eval')]
+        cgb_model = xgb.train(params_xgb, xgb_train, num_boost_round=3000, evals=evallist, verbose_eval=300, early_stopping_rounds=500)
         vaild_pre = cgb_model.predict(xgb_valid, ntree_limit=cgb_model.best_ntree_limit)
-        auc = myFevalAuc(vaild_pre,valid_label)
-        if auc<0.928:
-            continue
-        score.append(auc)
-        print("score: ",auc)
-        xgb_test = xgb.DMatrix(test_x)
+        sauc = myFevalAuc(vaild_pre,valid_label)
+        score.append(sauc)
+        print("iter %02d" % (iter+1),"score: ",sauc)
+        
+#         importance = cgb_model.get_score(importance_type="weight")
+#         tuples = [(k, importance[k]) for k in importance]
+#         tuples = sorted(tuples, key=lambda x: x[1])
+#         for i in tuples:
+#             print(i)
+#         print(len(tuples))
+#         plot_importance(cgb_model,max_num_features=50)
+#         plt.show()
+
+        xgb_test = xgb.DMatrix(test_x,silent=True)
         preds = cgb_model.predict(xgb_test, ntree_limit=cgb_model.best_ntree_limit)
         res.append(MinMaxScaler().fit_transform(preds.reshape(-1, 1)))
         print("\n")
-#         plot_importance(cgb_model,max_num_features=40)
-#         plt.show()
-    print("平均得分: ",sum(score)/len(score))
+
+    print("############################")
+    print("平均得分: ",sum(score)/len(score)," 模型数量: ",len(score))
     res = np.array(res)
     res = res.mean(axis=0)
     sub["Label"] = res
@@ -225,40 +287,29 @@ def xgb_model():
 def myFevalAuc(preds, xgb_label):
     fpr, tpr, _ = roc_curve(xgb_label, preds, pos_label=1)
     return auc(fpr, tpr)
-
-def read_x():
-#     workbook=xlrd.open_workbook("data/auc.xls")
-    df = pd.read_csv('data/auc1.csv', encoding='utf-8',names=['A', 'B'])
-    df = df.sort_values(by="B",ascending=False)
-    print(df.head(2000))
     
 if __name__ == "__main__":
     print("start")
-    params_xgb = {
-        'booster':'gbtree',
-        'objective': 'binary:logistic',
-        'eval_metric':'auc',
-        
-#         'gamma':0.2,        #损失下降多少才分裂
-        'min_child_weight':3,
-        'max_depth':8,
-        'lambda':3,     #控制模型复杂度的权重值的L2曾泽化参数，参数越大越不容易过拟合
-        'subsample':0.85,    #随机采样的训练样本
-        'colsample_bytree':0.7, #生成树时特征采样比例
-        'colsample_bylevel':0.75, #表示树的每层划分的特征采样比例
-        'eta': 0.03,
-        
-        'tree_method':'hist', #表示树的构建方法，确切来说是切分点的选择算法，'gpu_hist' or 'gpu_exact'包括贪心算法，近似贪心算法，直方图算法
-        'seed':1000,
-        'silent': 0,    #信息输出设置成1则没有信息输出
-        'nthread':12
-        }
+    df1 = pd.read_csv("user_data/lgb.csv", encoding='utf-8')
+    df1.rename(columns={'Label':'lgb'},inplace=True)
+    df2 = pd.read_csv("user_data/122.csv", encoding='utf-8')
+    df2.rename(columns={'Label':'122'},inplace=True)
+    df3 = pd.read_csv("user_data/sub.csv", encoding='utf-8')
+    df3.rename(columns={'Label':'sub'},inplace=True)
+    df1 = pd.merge(df1,df2,on=['ID'],how='right')
+    df1 = pd.merge(df1,df3,on=['ID'],how='right')
+    print(df1.corr(method='pearson', min_periods=1))
+#     df1["Label"] = 0.55*df1["Label1"]+0.45*df1["Label2"]
+#     df1.drop(["Label1","Label2"],axis=1,inplace=True)
+#     df1.to_csv('com_xgb_lgb2.csv', index=False,encoding='utf-8') 
+    print(df1.head())
+
 #     data_deal()
-    xgb_model()
+#     xgb_model()
 #     data_review()
 #     read_x()
     
     
-    
+        
     
     
